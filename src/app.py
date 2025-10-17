2fbc477ebace36ad6e8062ffdf41b2e816e860ee
Laura Montes <laumoen@gmail.com>
2025-10-17 17:58:45 +0000

    successful endpoints

--- src/app.py
+++ src/app.py
@@ -1,14 +1,11 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
+from admin import setup_admin
 from models import db, User, Planet, Character, Fav, Weapon
-# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -21,55 +18,20 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Handle/serialize errors like a JSON object
+MIGRATE = Migrate(app, db)
+db.init_app(app)
+CORS(app)
+setup_admin(app)
 
 
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-
 
 @app.route('/')
-def sw_sitemap():
-    return jsonify({"mensaje": "Welcome to the Star Wars world!"})
-
-
-@app.route('/sitemap', methods=['GET'])
-def get_starwars():
-    return jsonify(sitemap)
-
-
-@app.route('/sitemap', methods=['POST'])
-def post_starwars():
-    nueva = request.get_json()
-    nueva["id"] = len(sitemap) + 1
-    sitemap.append(nueva)
-    return jsonify(nueva), 201
-
-
-@app.route('/sitemap/<int:id>', methods=['DELETE'])
-def delete_starwars(id):
-    global sitemap
-    sitemap = [t for t in sitemap if t["id"] != id]
-    return jsonify({"mensaje": f"Sitemap {id} eliminated."})
-
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
-
-
-@app.route('/user', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([user.serialize() for user in users]), 200
-
-
-@app.route('/planet', methods=['GET'])
-def get_planets():
-    planets = Planet.query.all()
-    return jsonify([planet.serialize() for planet in planets]), 200
+def starwars():
+    return jsonify({"mensaje": "welcome to the api far far away! :)"})
 
 
 @app.route('/character', methods=['GET'])
@@ -78,10 +40,49 @@ def get_characters():
     return jsonify([character.serialize() for character in characters]), 200
 
 
-@app.route('/fav', methods=['GET'])
-def get_favs():
-    favs = Fav.query.all()
-    return jsonify([fav.serialize() for fav in favs]), 200
+@app.route('/character', methods=['POST'])
+def post_character():
+    data = request.get_json()
+    required_fields = ["name", "last_name", "race", "native_planet", "is_jedi"]
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Faltan campos obligatorios"}), 400
+
+    new_character = Character(
+        name=data["name"],
+        last_name=data["last_name"],
+        race=data["race"],
+        native_planet=data["native_planet"],
+        is_jedi=data["is_jedi"]
+    )
+
+    db.session.add(new_character)
+    db.session.commit()
+
+    return jsonify(new_character.serialize()), 201
+
+
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.serialize() for planet in planets]), 200
+
+
+@app.route('/planet', methods=['POST'])
+def post_planet():
+    data = request.get_json()
+    required_fields = ["name", "solar_system"]
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Faltan campos obligatorios"}), 400
+
+    new_planet = Planet(
+        name=data["name"],
+        solar_system=data["solar_system"]
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify(new_planet.serialize()), 201
 
 
 @app.route('/weapon', methods=['GET'])
@@ -90,7 +91,29 @@ def get_weapons():
     return jsonify([weapon.serialize() for weapon in weapons]), 200
 
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/weapon', methods=['POST'])
+def post_weapon():
+    data = request.get_json()
+    required_fields = ["type", "name", "is_lethal", "weapon_owner_id"]
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Faltan campos obligatorios"}), 400
+
+    new_weapon = Weapon(
+        type=data["type"],
+        name=data["name"],
+        is_lethal=data["is_lethal"],
+        weapon_owner_id=data["weapon_owner_id"]
+    )
+
+    db.session.add(new_weapon)
+    db.session.commit()
+
+    return jsonify(new_weapon.serialize()), 201
+
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
